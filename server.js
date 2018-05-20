690c4fe6af29f8d633521fcc44e270a842f56aa7
Ramnath <Ramnath.ananth@gmail.com>
2018-05-20 23:20:16 +0800

    modified server.js

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // modules =================================================
 var express        = require('express');
+var app            = express();
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
