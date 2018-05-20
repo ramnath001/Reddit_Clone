9e3d2b08c0df47849b9e270443efcabdee75e5a0
Ramnath <Ramnath.ananth@gmail.com>
2018-05-20 23:14:54 +0800

    mongoose dependency deleted

--- server.js
+++ server.js
@@ -1,7 +1,5 @@
 // modules =================================================
 var express        = require('express');
-var app            = express();
-var mongoose       = require('mongoose');
 var bodyParser     = require('body-parser');
 var methodOverride = require('method-override');
 
