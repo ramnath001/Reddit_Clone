fe0b2090d8022ecc2ab96ea6d6f7a757791e5ce4
Ramnath <Ramnath.ananth@gmail.com>
2018-05-30 00:42:21 +0800

    refactored app.js

--- public/app.js
+++ public/app.js
@@ -21,7 +21,7 @@ angular.module("RedditCtlr", ["ngAnimate"]).controller("RedditController", [
 /*
  * This function calls the create post service which intiates an http request to the server
  */
-    $scope.createNewPost = function createNewPost() {
+  $scope.createNewPost = function createNewPost() {
       console.log($scope.newPostForm);
 
       postService.createPost($scope.newPostForm).success(function(data) {
@@ -37,7 +37,7 @@ angular.module("RedditCtlr", ["ngAnimate"]).controller("RedditController", [
   /*
  * This function calls the create comment service which intiates an http request to the server
  */
-    $scope.createNewComment = function createNewComment(pid) {
+  $scope.createNewComment = function createNewComment(pid) {
       var newComment = {};
       newComment.commenter = this.newCommentForm.commenter;
       newComment.commentBody = this.newCommentForm.commentBody;
@@ -50,7 +50,11 @@ angular.module("RedditCtlr", ["ngAnimate"]).controller("RedditController", [
       $scope.showNewPostForm = false;
     };
 
-    $scope.addPoints = function addPoints(pid,action) {
+/*
+ * This function calls the create manipulatePoints service which intiates an http request to the server to increase points
+ */ 
+
+  $scope.addPoints = function addPoints(pid,action) {
       console.log(pid);
 
       postService.manipulatePoints(pid,action).success(function(data) {
@@ -63,38 +67,11 @@ angular.module("RedditCtlr", ["ngAnimate"]).controller("RedditController", [
       });
     };
 
-    $scope.toggleNewCommentsForm = function toggleNewCommentsForm() {
-      if (this.showNewCommentForm) {
-        this.showNewCommentForm = false;
-      } else {
-        this.showNewCommentForm = true;
-      }
-    };
+/*
+ * This function calls the create manipulatePoints service which intiates an http request to the server to decrease points
+ */ 
 
-   
-    $scope.toggleComments = function toggleComments(pid) {
-      console.log(pid);
-      for (var i in $scope.posts) {
-        if ($scope.posts[i].id == pid) {
-          $scope.posts[i].showComments = true;
-        } else {
-          $scope.posts[i].showComments = false;
-        }
-      }
-      console.log($scope.posts);
-    };
-
-    
-    $scope.toggleNewCommentsForm = function toggleNewCommentsForm() {
-      if (this.showNewCommentForm) {
-        this.showNewCommentForm = false;
-      } else {
-        this.showNewCommentForm = true;
-      }
-    };
-
-   
-    $scope.subtractPoints = function subtractPoints(pid,action) {
+  $scope.subtractPoints = function subtractPoints(pid,action) {
       console.log(pid);
 
       postService.manipulatePoints(pid,action).success(function(data) {
@@ -107,6 +84,38 @@ angular.module("RedditCtlr", ["ngAnimate"]).controller("RedditController", [
       });
     };
 
-    $scope.sortBy = "-points";
+
+  $scope.toggleNewCommentsForm = function toggleNewCommentsForm() {
+      if (this.showNewCommentForm) {
+        this.showNewCommentForm = false;
+      } else {
+        this.showNewCommentForm = true;
+      }
+    };
+
+   
+  $scope.toggleComments = function toggleComments(pid) {
+      console.log(pid);
+      for (var i in $scope.posts) {
+        if ($scope.posts[i].id == pid) {
+          $scope.posts[i].showComments = true;
+        } else {
+          $scope.posts[i].showComments = false;
+        }
+      }
+      console.log($scope.posts);
+    };
+
+    
+  $scope.toggleNewCommentsForm = function toggleNewCommentsForm() {
+      if (this.showNewCommentForm) {
+        this.showNewCommentForm = false;
+      } else {
+        this.showNewCommentForm = true;
+      }
+    };
+
+  $scope.sortBy = "-points";
+  
   }
 ]);
